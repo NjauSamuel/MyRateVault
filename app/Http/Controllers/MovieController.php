4a26972444e98f57f8a48d41c6f910e49298546c
NjauSamuel <njaus602@gmail.com>
2024-09-18 19:13:07 +0300

    Fixed a bug in the movie controller, now the data is being fetched from cache.

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 
 class MovieController extends Controller
@@ -36,27 +37,36 @@ class MovieController extends Controller
 
     public function index()
     {
-        // Fetch movies from the API
-        $response = Http::withOptions(['verify' => false])->get('https://api.themoviedb.org/3/trending/all/day?language=en-US', [
-            'headers' => [
-                'accept' => 'application/json',
-            ],
-            'api_key' => env('MOVIE_DB_API_KEY') // Fetched from the .env file
-        ]);
+        // Cache the entire content of the index method for 6 hours (360 minutes)
+        $movies = Cache::remember('movies_with_trailers', 360, function () {
+            // Fetch movies from the API
+            $response = Http::withOptions(['verify' => false])->get('https://api.themoviedb.org/3/trending/all/day?language=en-US', [
+                'headers' => [
+                    'accept' => 'application/json',
+                ],
+                'api_key' => env('MOVIE_DB_API_KEY') // Fetched from the .env file
+            ]);
 
-        $movies = $response->json('results');
+            $movies = $response->json('results');
 
-        // For each movie/TV show, fetch the trailer
-        foreach ($movies as &$movie) {
-            $title = $movie['title'] ?? $movie['name']; // Use title for movies and name for TV shows
-            $year = substr($movie['release_date'] ?? $movie['first_air_date'], 0, 4); // Extract the year
-            // $movie['trailer_url'] = $this->getTrailer($title, $year);            
-            $movie['trailer_url'] = "https://www.youtube.com/embed/lU_NKNZljoQ?si=RssBRMU5do2A2sAK";
-        }
+            // For each movie/TV show, fetch the trailer
+            foreach ($movies as &$movie) {
+
+                // Skip if the media type is 'person'
+                if ($movie['media_type'] === 'person') {
+                    continue;
+                }
+
+                $title = $movie['title'] ?? $movie['name']; // Use title for movies and name for TV shows
+                $year = substr($movie['release_date'] ?? $movie['first_air_date'], 0, 4); // Extract the year
+                $movie['trailer_url'] = $this->getTrailer($title, $year);            
+            }
+
+
+            return $movies;
+        });
 
         // Return the view with movies and their trailers
         return view('home.index', ['movies' => $movies]);
     }
-
-    
 }
